c9fe41e565d87dad197f6a187ca622725270a83b
kevingandrades <kevingandrades@gmail.com>
2025-10-20 21:12:36 -0400

    refactor: platform path detection

--- src/prisma.rs
+++ src/prisma.rs
@@ -1,7 +1,6 @@
 use std::{env, fs};
 use zed_extension_api::{self as zed, serde_json, Result};
 
-const SERVER_PATH: &str = "node_modules/.bin/prisma-language-server";
 const PACKAGE_NAME: &str = "@prisma/language-server";
 
 struct PrismaExtension {
@@ -9,14 +8,23 @@ struct PrismaExtension {
 }
 
 impl PrismaExtension {
-    fn server_exists(&self) -> bool {
-        fs::metadata(SERVER_PATH).map_or(false, |stat| stat.is_file())
+    fn server_exists(&self, server_path: &str) -> bool {
+        fs::metadata(server_path).map_or(false, |stat| stat.is_file())
     }
 
     fn server_script_path(&mut self, language_server_id: &zed::LanguageServerId) -> Result<String> {
-        let server_exists = self.server_exists();
+        let (os, _arch) = zed::current_platform();
+        let server_path = format!(
+            "node_modules/{server_script}",
+            server_script = match os {
+                zed::Os::Mac | zed::Os::Linux => ".bin/prisma-language-server",
+                zed::Os::Windows => "@prisma/language-server/dist/bin.js"
+            }
+        );
+
+        let server_exists = self.server_exists(&server_path);
         if self.did_find_server && server_exists {
-            return Ok(SERVER_PATH.to_string());
+            return Ok(server_path);
         }
 
         zed::set_language_server_installation_status(
@@ -35,14 +43,14 @@ impl PrismaExtension {
             let result = zed::npm_install_package(PACKAGE_NAME, &version);
             match result {
                 Ok(()) => {
-                    if !self.server_exists() {
+                    if !self.server_exists(&server_path) {
                         Err(format!(
-                            "installed package '{PACKAGE_NAME}' did not contain expected path '{SERVER_PATH}'",
+                            "installed package '{PACKAGE_NAME}' did not contain expected path '{server_path}'",
                         ))?;
                     }
                 }
                 Err(error) => {
-                    if !self.server_exists() {
+                    if !self.server_exists(&server_path) {
                         Err(error)?;
                     }
                 }
@@ -50,7 +58,7 @@ impl PrismaExtension {
         }
 
         self.did_find_server = true;
-        Ok(SERVER_PATH.to_string())
+        Ok(server_path)
     }
 }
 
@@ -82,7 +90,7 @@ impl zed::Extension for PrismaExtension {
         language_server_id: &zed::LanguageServerId,
         _worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
-        let server_path = zed_ext::resolve_windows_path(self.server_script_path(language_server_id)?);
+        let server_path = self.server_script_path(language_server_id)?;
         Ok(zed::Command {
             command: zed::node_binary_path()?,
             args: vec![
@@ -99,16 +107,3 @@ impl zed::Extension for PrismaExtension {
 }
 
 zed::register_extension!(PrismaExtension);
-
-mod zed_ext {
-    pub fn resolve_windows_path(path: String) -> String {
-        use zed_extension_api::{current_platform, Os};
-
-        let (os, _arch) = current_platform();
-
-        match os {
-            Os::Mac | Os::Linux => path,
-            Os::Windows => "node_modules/@prisma/language-server/dist/bin.js".to_string()
-        }
-    }
-}
