1343a250a3333ec287f5a96b86f3e7eb7477380f
MrSubidubi <dev@bahn.sh>
2026-07-31 19:56:21 +0200

    Format Tree-sitter queries

--- languages/prisma/highlights.scm
+++ languages/prisma/highlights.scm
@@ -1,42 +1,88 @@
 [
- "datasource"
- "enum"
- "generator"
- "model"
- "view"
- "type"
+  "datasource"
+  "enum"
+  "generator"
+  "model"
+  "view"
+  "type"
 ] @keyword
 
 (comment) @comment
+
 (developer_comment) @comment
 
 (number) @number
+
 (string) @string
+
 (false) @boolean
+
 (true) @boolean
+
 (arguments) @property
+
 (maybe) @punctuation
-(call_expression (identifier) @function)
+
+(call_expression
+  (identifier) @function)
+
 (enumeral) @constant
+
 (identifier) @variable
-(column_type (identifier) @type)
-(column_type (call_expression (identifier) @type))
+
+(column_type
+  (identifier) @type)
+
+(column_type
+  (call_expression
+    (identifier) @type))
+
 (type_declaration_type) @type
-(type_declaration (identifier) @type.definition)
-(column_declaration (identifier) (column_type (identifier) @type))
-(attribute (identifier) @label)
-(attribute (call_expression (identifier) @label))
-(attribute (call_expression (member_expression (identifier) @label)))
-(block_attribute_declaration (identifier) @label)
-(block_attribute_declaration (call_expression (identifier) @label))
-(type_expression (identifier) @property)
+
+(type_declaration
+  (identifier) @type.definition)
+
+(column_declaration
+  (identifier)
+  (column_type
+    (identifier) @type))
+
+(attribute
+  (identifier) @label)
+
+(attribute
+  (call_expression
+    (identifier) @label))
+
+(attribute
+  (call_expression
+    (member_expression
+      (identifier) @label)))
+
+(block_attribute_declaration
+  (identifier) @label)
+
+(block_attribute_declaration
+  (call_expression
+    (identifier) @label))
+
+(type_expression
+  (identifier) @property)
 
 "(" @punctuation.bracket
+
 ")" @punctuation.bracket
+
 "[" @punctuation.bracket
+
 "]" @punctuation.bracket
+
 "{" @punctuation.bracket
+
 "}" @punctuation.bracket
+
 "=" @operator
+
 "@" @operator
+
 "@@" @operator
--- languages/prisma/outline.scm
+++ languages/prisma/outline.scm
@@ -1,44 +1,38 @@
 ; Datasource blocks
 (datasource_declaration
-    "datasource" @context
-    (identifier) @name
-    (statement_block)
-) @item
+  "datasource" @context
+  (identifier) @name
+  (statement_block)) @item
 
 ; Models
 (model_declaration
-    "model" @context
-    (identifier) @name
-    (statement_block)
-) @item
+  "model" @context
+  (identifier) @name
+  (statement_block)) @item
 
 ; Views
 (view_declaration
-    "view" @context
-    (identifier) @name
-    (statement_block)
-) @item
+  "view" @context
+  (identifier) @name
+  (statement_block)) @item
 
 ; Generator blocks
 (generator_declaration
-    "generator" @context
-    (identifier) @name
-    (statement_block)
-) @item
+  "generator" @context
+  (identifier) @name
+  (statement_block)) @item
 
 ; Types
 (type_declaration
-    "type" @context
-    (identifier) @name
-    (statement_block)
-) @item
+  "type" @context
+  (identifier) @name
+  (statement_block)) @item
 
 ; Enums
 (enum_declaration
-    "enum" @context
-    (identifier) @name
-    (enum_block)
-) @item
+  "enum" @context
+  (identifier) @name
+  (enum_block)) @item
 
 ; Comments
 (comment) @documentation
